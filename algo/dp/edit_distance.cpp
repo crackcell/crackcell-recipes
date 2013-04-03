84d4fd60a0fcd12450ef0e546fe543b12b1a124e
Menglong Tan <tanmenglong@gmail.com>
2013-04-04 04:32:10 +0800

    u

--- algo/dp/edit_distance.cpp
+++ algo/dp/edit_distance.cpp
@@ -28,8 +28,9 @@
 char m[MAX_LEN][MAX_LEN];
 
 void print_m(const int len1, const int len2) {
-    for (int i = 0; i < len1; i++) {
-        for (int j = 0; j < len2; j++) {
+    printf("matrix:\n");
+    for (int i = 0; i <= len1; i++) {
+        for (int j = 0; j <= len2; j++) {
             printf("%d ", m[i][j]);
         }
         printf("\n");
@@ -48,32 +49,35 @@ int edit_distance(const char *s1, const char *s2) {
     int len1 = strlen(s1);
     int len2 = strlen(s2);
 
-    for (int i = 0; i < len1; i++) {
+    for (int i = 0; i <= len1; i++) {
         m[i][0] = i;
     }
 
-    for (int j = 0; j < len2; j++) {
+    for (int j = 0; j <= len2; j++) {
         m[0][j] = j;
     }
 
-    for (int i = 1; i < len1; i++) {
-        for (int j = 1; j < len2; j++) {
+    for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
             // 1. replace char: s1[i] to s2[j]
             // 2. delete char: s1[i]
             // 3. delete char: s2[i]
             m[i][j] = min(m[i - 1][j - 1] + (s1[i] == s2[j] ? 0 : 1),
                           m[i - 1][j] + 1,
                           m[i][j - 1] + 1);
+            print_m(len1, len2);
         }
     }
 
-    print_m(len1, len2);
-    return m[len1 - 1][len2 - 1];
+    //print_m(len1, len2);
+    return m[len1][len2];
 }
 
 int main(int argc, char *argv[]) {
-    const char *s1 = "asdfwefsdfasdfwefx";
-    const char *s2 = "asdf234r24323sadf";
+    //const char *s1 = "asgdfg124234";
+    //const char *s2 = "werfs1";
+    const char *s1 = "b21";
+    const char *s2 = "1";
     printf("edit distance: %d\n", edit_distance(s1, s2));
     return 0;
 }
