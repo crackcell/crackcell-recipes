9008abac817461d30ccb394f530dd9845de4638d
Menglong Tan <tanmenglong@gmail.com>
2013-06-10 08:15:40 +0800

    u

---
+++ gsl-test/gsl_marco.cpp
@@ -0,0 +1,32 @@
+/* -*- encoding: utf-8; indent-tabs-mode: nil -*- */
+
+/***************************************************************
+ *
+ * Copyright (c) 2013, Tan Menglong <tanmenglong@gmail.com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GPL licence
+ *
+ **************************************************************/
+
+/**
+ * 
+ *
+ * @file gsl_marco.cpp
+ * @author Menglong TAN <tanmenglong@gmail.com>
+ * @date Mon Jun 10 07:58:30 2013
+ *
+ **/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+    printf("%f\n", M_E);
+    printf("%f\n", M_LOG2E);
+    printf("%f\n", M_LOG10E);
+    printf("%f\n", M_LN2);
+    return 0;
+}
+
+/* vim: set expandtab shiftwidth=4 tabstop=4: */
